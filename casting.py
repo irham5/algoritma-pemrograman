364c7e94d34edf3ca7998002b8016828fcb6ee68
nahruliam <irhamnahrul344@gmail.com>
2025-03-12 08:24:08 +0700

    Create casting.py

---
+++ casting.py
@@ -0,0 +1,10 @@
+# input
+lanang = int(input('jumlah anggota lanang : '))
+perempuan = int(input('jumlah anggota perempuan : '))
+
+# proses 
+total = lanang + perempuan
+
+# output 
+print('hasil penjumlahan :',total)
+print('-----------------------')
