1c14d047c0c236af3ead5bc4589ee7a60fa1b717
nahruliam <irhamnahrul344@gmail.com>
2025-03-12 08:20:22 +0700

    Create typedata.py

---
+++ typedata.py
@@ -0,0 +1,28 @@
+# string
+divisi = 'kimia'
+
+# integer (bilangan bulat)
+jumblah_anggota = 5
+
+# float(bilangan koma)
+rata_rata_nilai = 90.00
+
+# list
+nama = ['rina', 'aurel', 'ken', 'star', 'angkasa']
+
+#tuple
+kelas = ('IPA1', 'IPA2', 'IPA3', 'IPA4')
+
+#set
+hak_peserta = {'bimbel', 'transport' 'konsumsi'}
+
+#dictionary
+data_peserta = {'nama': 'rina', 'kelas' : 'IPA2'}
+
+# tampilkan tipe data
+print('data 1 type datanya : ', type(divisi))
+print('data 2 type datanya : ', type(jumblah_anggota))
+print('data 3 type datanya : ', type(rata_rata_nilai))
+print('data 5 type datanya : ', type(kelas))
+print('data 6 type datanya : ', type(hak_peserta))
+print('data 7 type datanya : ', type(data_peserta))
