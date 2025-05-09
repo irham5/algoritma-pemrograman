8c5a2ce2536d9b1d104265571582845d2e61689a
nahrulirham <irhamnahrul344@gmail.com>
2025-05-09 19:36:56 +0700

    Create whileloop.py

---
+++ whileloop.py
@@ -0,0 +1,8 @@
+#print('Pengulangan ke : 1')
+#print('Pengulangan ke : 2')
+#print('Pengulangan ke : 3')
+
+nilaiawal = 1
+while nilaiawal <= 1000 :
+    print('pengulangan ke :',nilaiawal)
+    nilaiawal += 1
