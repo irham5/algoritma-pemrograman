ae8095c01105ee86d9482d7cf47e87cf6b7db0f1
nahrulirham <irhamnahrul344@gmail.com>
2025-05-09 19:07:48 +0700

    Create ifelse.py

---
+++ ifelse.py
@@ -0,0 +1,12 @@
+#input
+print('HASIL TES CPNS')
+nilai = int(input('masukkan nilai : '))
+
+#proses
+if nilai >= 350 :
+    status = 'lulus'
+else :
+    status = 'gagal'
+
+#output
+print('status :',status)
