ea9f44975be9abf87e1eabbeab9294746d040e40
nahrulirham <irhamnahrul344@gmail.com>
2025-05-09 19:29:25 +0700

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,9 @@
+print('FOR IN SEQUENCE')
+nama = ['kont','paul','rahma','jojo']
+for data in nama:
+    print('nama :',data)
+print('-------------')
+
+print('FOR IN RANGE')
+for nilai in range(1,7) :
+    print('pengulangan ke :',nilai)
