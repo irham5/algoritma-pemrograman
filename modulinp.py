9f42c5f85a50d036dafd5f1c3759352f1c33a61d
nahrulirham <irhamnahrul344@gmail.com>
2025-05-12 19:14:19 +0700

    Create modulinp.py

---
+++ modulinp.py
@@ -0,0 +1,11 @@
+def jumblah(angka1,angka2):
+    hasil = angka1 + angka2
+    print('hasil penjumlahan :',hasil)
+jumblah(5,7)
+print('--------------------------------')
+
+def kali(angka1,angka2):
+    hasil = angka1 * angka2
+    print('hasil perkalian :',hasil)
+kali(4,5)
+print('---------------------------------')
